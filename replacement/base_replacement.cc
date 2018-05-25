b51b84ec4291c98a14de36afada1eac2ee201ba1
Chaitanya Deshpande <cdeshpan@xlnx.xilinx.com>
2018-05-25 08:20:10 -0700

    Changes2 done

--- replacement/base_replacement.cc
+++ replacement/base_replacement.cc
@@ -11,6 +11,7 @@ uint32_t CACHE::find_victim(uint32_t cpu, uint64_t instr_id, uint32_t set, const
 void CACHE::update_replacement_state(uint32_t cpu, uint32_t set, uint32_t way, uint64_t full_addr, uint64_t ip, uint64_t victim_addr, uint32_t type, uint8_t hit)
 {
 	int x = 0;
+	int y = 0;
 	
     if (type == WRITEBACK) {
         if (hit) // wrietback hit does not update LRU state
